04b966c2e380356faae6624bac947c3b93a139d5
heriswn <heri15002@mail.unpad.ac.id>
2021-11-03 06:59:44 +0700

    add comment policy

---
+++ src/pages/comment-policy.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Comment Policy">
+      <p>We welcome relevant and respectful comments, Off-topic commencts may be removed..</p>
+    </Layout>
+  )
+}
+
+export default IndexPage
