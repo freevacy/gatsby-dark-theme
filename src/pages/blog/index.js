bdf6f9c9a22dfbaadbb37422e265ae9fc53427be
heriswn <heri15002@mail.unpad.ac.id>
2021-11-02 19:43:34 +0700

    slug blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,24 +1,22 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import Layout from '../../components/layout'
+import { Link, graphql } from 'gatsby'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
         data.allMdx.nodes.map((node) => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <MDXRenderer>
-              {node.body}
-            </MDXRenderer>
           </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
@@ -32,7 +30,7 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
