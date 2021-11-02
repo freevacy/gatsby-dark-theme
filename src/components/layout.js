9e7f7e5c9b0c6d795ed883899e60ba3739b8da99
heriswn <heri15002@mail.unpad.ac.id>
2021-11-02 18:43:22 +0700

    add blog

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,32 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle
 } from './layout.module.css'
 
 const Layout = (props) => {
   const pageTitle = props.pageTitle
   const children = props.children
   //ini teh pagetitle children teh props
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
 
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
@@ -26,6 +37,11 @@ const Layout = (props) => {
           <li className={navLinkItem}>
             <Link to="/about" className={navLinkText}>
               About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
             </Link>
           </li>
         </ul>
