8640341f09e6c95e977abed7bce4555a5516d627
heriswn <heri15002@mail.unpad.ac.id>
2021-11-02 18:18:37 +0700

    add layout

---
+++ src/components/layout.js
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+import {
+  container,
+  heading,
+  navLinks,
+  navLinkItem,
+  navLinkText
+} from './layout.module.css'
+
+const Layout = (props) => {
+  const pageTitle = props.pageTitle
+  const children = props.children
+  //ini teh pagetitle children teh props
+
+  return (
+    <div className={container}>
+      <title>{pageTitle}</title>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,11 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react'
+import Layout from '../components/layout'
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-
-// markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home | Freevacy</title>
-      <h1 style={headingStyles}>
-        <h1>FREEVACY</h1>
-        <br />
-        <span style={headingAccentStyles}>— about privacy tips! Coming Soon... </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <h3>
-        <Link to="/404" style={{textDecoration: "none"}}>Error Page</Link>
-      </h3>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
   )
 }
 
