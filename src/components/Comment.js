303b78b99ebe4bf2f44c78748f643977f8e37ae2
herionlinux <5sujmea6@duck.com>
2021-11-07 18:12:28 +0700

    improve navbar

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,23 +1,19 @@
 import React, {Component} from "react";
-import ToggleMode from "./toggler";
 
 export default class Comments extends Component {
-  static contextType = ToggleMode;
 
   constructor(props){
     super(props);
     this.commentBox = React.createRef(); // Creates a reference to inject the <script> element
   }
   componentDidMount () {
-      const theme = this.context;
-      const utteranceTheme = theme.dark ? "github-dark" : "github-light";
       let scriptEl = document.createElement("script");
       scriptEl.setAttribute("src", "https://utteranc.es/client.js");
       scriptEl.setAttribute("crossorigin","anonymous");
       scriptEl.setAttribute("async", true);
       scriptEl.setAttribute("repo", "heriswn/freevacy-comments");
       scriptEl.setAttribute("issue-term", "pathname");
-      scriptEl.setAttribute( "theme", utteranceTheme);
+      scriptEl.setAttribute( "theme", "github-light");
       this.commentBox.current.appendChild(scriptEl);
   }
 
